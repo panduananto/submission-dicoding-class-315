16649fa745e699489e2bc8a11107c5ea1a63374e
panduananto <pandunih@gmail.com>
2022-06-09 09:59:42 +0700

    add prettier config, create form element

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 // https://assets.justinmind.com/wp-content/webp-express/webp-images/uploads/2019/10/list-ui-design-cflow-journal.png.webp
+function isStorageExist() {
+  if (typeof Storage === undefined) {
+    alert('Browser tidak mendukung local storage');
+    return false;
+  }
+
+  return true;
+}
+
+function loadDataFromLocalStorage() {}
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (isStorageExist()) {
+    loadDataFromLocalStorage();
+  }
+});
